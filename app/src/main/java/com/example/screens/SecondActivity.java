cc527567704bd129acd77096692c36d675db9839
Hugo <a21hugni@student.his.se>
2022-04-25 23:05:48 +0200

    Lade till intents och data som ska skickas till den SecondActivity

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -3,12 +3,17 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        text = findViewById(R.id.textView);
     }
 }
